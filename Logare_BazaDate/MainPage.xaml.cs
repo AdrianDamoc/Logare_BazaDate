6cfc62b5109c47f2003839563e8e72cfecb65b3d
Adrian <knifeofficial@gmail.com>
2020-02-04 10:34:09 +0200

    Add project files.

---
+++ Logare_BazaDate/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data;
+using Windows.UI.Popups;
+using Logare_BazaDate.Views;
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Logare_BazaDate
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RegisterID_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Pagina2));
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=82.208.137.149\sqlexpress,8833;Initial Catalog=proba_transare;Persist Security Info=True;User ID=sa;Password=pro";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            string sql = "Select utilizator,parola from Tabel_Utilizatori where utilizator = '" + utilizatorbox.Text + "' and parola ='" + parolabox.Password+"'";
+
+            command = new SqlCommand(sql, cnn);
+
+            String strResult = String.Empty;
+           // int length = strResult.Length;
+            strResult = (String)command.ExecuteScalar();
+            if (strResult == null)
+            {
+               
+                ContentDialog dialog = new ContentDialog()
+                {
+                    Title = "Mesaj",
+                    MaxWidth = this.ActualWidth,
+                    PrimaryButtonText = "OK",
+                    Content = new TextBlock
+                    {
+                        Text = "Nu esti inregistrat",
+                        FontSize = 24,
+
+
+
+                    },
+
+                };
+                await dialog.ShowAsync();
+
+            } 
+            else
+            { 
+                ContentDialog dialog = new ContentDialog()
+                {
+                    Title = "Mesaj",
+                    MaxWidth = this.ActualWidth,
+                    PrimaryButtonText = "OK",
+                    Content = new TextBlock
+                    {
+                        Text = "Te-ai logat",
+                        FontSize = 24,
+
+
+
+                    },
+
+                };
+                await dialog.ShowAsync();
+
+            }
+
+
+            command.Dispose();
+
+   
+
+
+            cnn.Close();
+        }
+
+
+
+
+
+
+      
+        }
+       
+    }
+
---
+++ Logare_BazaDate/Views/Pagina2.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Logare_BazaDate.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Pagina2 : Page
+    {
+        public Pagina2()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void RegisterNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=82.208.137.149\sqlexpress,8833;Initial Catalog=proba_transare;Persist Security Info=True;User ID=sa;Password=pro";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            string sql = "Insert into Tabel_Utilizatori (utilizator,parola) values" + " ('" + UserRegID.Text.Trim()+"','"+  PassRegID.Text.Trim() + "')";
+
+            command = new SqlCommand(sql, cnn);
+            adapter.InsertCommand = new SqlCommand(sql, cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+            
+           
+
+            
+
+            command.Dispose();
+
+            ContentDialog dialog = new ContentDialog()
+            {
+                Title = "Mesaj",
+                MaxWidth = this.ActualWidth,
+                PrimaryButtonText = "OK",
+                Content = new TextBlock
+                {
+                    Text = "Ai inserat in baza de date",
+                    FontSize = 24,
+                    
+                    
+
+        },
+
+            };
+            await dialog.ShowAsync();
+            
+
+            cnn.Close();
+        }
+
+    }
+}
